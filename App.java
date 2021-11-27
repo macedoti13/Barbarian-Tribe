6b9c1732bbf1411d955d2a7b90594fc1f1bf57ee
Thiago Macedo <thialmacedo@gmail.com>
2021-11-27 05:43:40 -0300

    App reads file

--- App.java
+++ App.java
@@ -6,13 +6,31 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class App {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
+
         Path path1 = Paths.get("TestFiles/exemplo.txt");
-        
+
         try (BufferedReader reader = Files.newBufferedReader(path1, Charset.defaultCharset())) {
-            
+            // Cria o objeto arvore
+            GeneralTree arvore = new GeneralTree();
+            // Variaveis 
+            String line;
+            String aux[];
+            // Adicionamos os primeiros dois elementos
+            arvore.LeInicio(reader.readLine(), reader.readLine());
+            // Lemos o resto da arvore e adicionamos os guerreiros
+            while ((line = reader.readLine()) != null) {
+                aux = line.split(" ");
+                arvore.add(aux[1], Integer.parseInt(aux[2]), aux[0]);
+            }
+            // Adicionamos a heranca de terras para cada filho
+            arvore.heranca();
+            // Geramos o diagrama da arvore
+            arvore.geraDOT();
+
+
         } catch (IOException e) {
             System.err.format("Erro na leitura do arquivo: ", e);
-        }
+        }  
     }
 }
